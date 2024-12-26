51829cc0d2a2d321b2fcd1fe132ff29dec7f9fcc
nsalkarka <_karka67@windowslive.com>
2024-12-26 22:23:48 +0300

    Add project files.

---
+++ Cental.DataAccessLayer/Context/CentalContext.cs
@@ -0,0 +1,31 @@
+﻿using Cental.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.DataAccessLayer.Context
+{
+    public class CentalContext : DbContext 
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-LDFLC27\\SQLEXPRESS;database=CentalDb;integrated security=true;trustServerCertificate=true");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Process> Processes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+    }
+}
